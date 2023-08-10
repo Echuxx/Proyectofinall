7633df70ec4f67d2437ed61bc43d6406c7ee08d4
Echuxx <jesusaah87@gmail.com>
2023-08-09 23:18:54 -0300

    cambios2

--- js/main.js
+++
@@ -1,6 +0,0 @@
-const botonModoToggle = document.getElementById("modoToggle");
-const body = document.body;
-
-botonModoToggle.addEventListener("click", () => {
-  body.classList.toggle("dark-mode");
-});
